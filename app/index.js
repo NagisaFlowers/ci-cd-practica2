86bcb6a878ae13bbff1f6f99853895d0df806bed
Nagisa Flowers <nagisa.ryuko93@gmail.com>
2025-09-25 11:55:30 -0600

    Simular rollback con error

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('¡Hola desde CI/CD!');
-// ← Falta esta llave de cierre
+});
 
-app.listen(port, () => {
+// función mal escrita
+app.liste(port, () => {  // ← "liste" en lugar de "listen"
   console.log(`App escuchando en http://localhost:${port}`);
+});
