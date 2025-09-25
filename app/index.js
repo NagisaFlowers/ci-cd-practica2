dad693984d5eeb93f385297d9973d2a868cb15d7
Nagisa Flowers <nagisa.ryuko93@gmail.com>
2025-09-25 12:14:24 -0600

    Reparacion CI

--- app/index.js
+++ app/index.js
@@ -4,9 +4,8 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('¡Hola desde CI/CD!');
-})
+});
 
-// Falta el punto y coma y paréntesis
 app.listen(port, () => {
-  console.log(`App escuchando en http://localhost:${port}`)
-// Falta el paréntesis de cierre y punto y coma
+  console.log(`App escuchando en http://localhost:${port}`);
+});
